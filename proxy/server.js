af5ac923b11c1d955abbd17b2dd5dcf9ad0f0646
Aowei Chenqi <aoweichenqi@Aoweis-MacBook-Pro.local>
2021-03-08 19:33:43 -0800

    changed endpoint

--- proxy/server.js
+++ proxy/server.js
@@ -30,7 +30,7 @@ app.get('/products/:productId/summary', (req, res) => {
 
 });
 
-app.post('/products/:productId', (req, res) => {
+app.post('/products/', (req, res) => {
   let id = req.params.productId;
   console.log('here')
   axios.post(`${productServicePath}/products/`)
